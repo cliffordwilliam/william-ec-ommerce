8449b537d9c2244d2e05678fcc3db557cbacd019
Clifford William <ccliffordwilliam@gmail.com>
2024-02-13 16:09:07 +0800

    feat(init): connect express + prisma + supabase

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { db } from "./lib/db";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", async (req, res) => {
+  const obj = await db.user.findMany();
+  res.status(200).json({
+    message: "Users successfully get.",
+    obj,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`listen ${PORT}`);
+});
