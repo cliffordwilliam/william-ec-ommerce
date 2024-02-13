e4375092f846696da5c1bc34e00c1765a5febfcb
Clifford William <ccliffordwilliam@gmail.com>
2024-02-13 16:25:50 +0800

    fix(typo): vercel config json

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.get("/", async (req, res) => {
   const obj = await db.user.findMany();
