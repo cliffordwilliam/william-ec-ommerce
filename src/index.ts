e5bb1a6940e0f8b46aa89c0c36899ddedbd6eca4
Clifford William <ccliffordwilliam@gmail.com>
2024-02-13 16:16:27 +0800

    feat(vercel): add dynamic port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { db } from "./lib/db";
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
   const obj = await db.user.findMany();
